336eb153e0e1372262430cbd3ab5bb3fc889e26e
Gabriel Lopes <gabrieldasilva10@gmail.com>
2019-08-19 13:49:18 -0300

    tratamendo de texto implementado

--- execute.py
+++ execute.py
@@ -1,9 +1,9 @@
 import unicodedata
-
+import csv
 import nltk
 from nltk import RegexpTokenizer, stem
 
-tonkenizer = RegexpTokenizer(r'\w+')
+tokenizer = RegexpTokenizer(r'\w+')
 
 stop_words = nltk.corpus.stopwords.words('portuguese')
 
@@ -11,14 +11,33 @@ stop_words = nltk.corpus.stopwords.words('portuguese')
 stemmer = stem.RSLPStemmer()
 
 def preProcessarTexto(texto):
-    novo_texto = []
+    texto_limpo = []
 
-    for frase in texto:
-        frase = frase.strip()
+    for linha in texto:
+        linha = linha.lower().strip()
 
-        if frase != '':
-            frase_lower = unicodedata.normalize('NFD', frase.lower()).encode('ASCII', 'ignore').decode('UTF-8')
-            print(frase_lower)
+        if linha != '':
+
+          linha = unicodedata.normalize('NFD', linha).encode('ASCII', 'ignore').decode('UTF-8')
+
+          listaDeTokens = tokenizer.tokenize(linha)
+
+          listaDeTokens = list(set(listaDeTokens).difference(stop_words))
+
+          for index in range(len(listaDeTokens)):
+              listaDeTokens[index] = stemmer.stem(listaDeTokens[index])
+
+          texto_limpo.append(listaDeTokens)
+
+    return texto_limpo
 
 
-preProcessarTexto(input("Digite uma frase \n"))
+
+
+depressivas = open('./depressivas.txt', 'r', encoding="utf8")
+nao_depressivas = open('./nao-depressivas.txt', 'r', encoding="utf8")
+
+print(preProcessarTexto(depressivas))
+
+teste = input("Insira uma frase: \n")
+print(preProcessarTexto([teste]))
